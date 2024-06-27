daba55e35c35e6007eb8a5694c99bdc14995beb2
FoucauldE <foucauld.estignard@edu.devinci.fr>
2024-06-27 16:29:08 +0200

    measure tendency of fine tuned model to complete antecedents with consequents

---
+++ consequents_generation.py
@@ -0,0 +1,68 @@
+import os
+import pandas as pd
+import argparse
+from csc_lib.data_loader import load_models
+from csc_lib.data_processing import correct_literal_eval
+from csc_lib.generation import Generator
+from csc_lib.evaluation import measure_chances_generating_target
+from csc_lib.visualize import violin_plot
+from csc_lib.config import FT_MODEL_PATH, OUTPUT_PATH
+
+def main(association_rules_path, experiment_name, nb_tries, max_new_tokens, step, block_size, ft_model_path=FT_MODEL_PATH):
+
+    # Check if association rules path exists
+    if not os.path.isfile(association_rules_path):
+        raise FileNotFoundError(f"The specified path for association rules does not exists: {association_rules_path}")
+    
+    # Load annotation rules
+    df_rules = pd.read_csv(association_rules_path)
+    df_rules = correct_literal_eval(df_rules, ['antecedents', 'consequents'])
+
+    # Prepare fine-tuned model to complete new prompts
+    tokenizer, base_model, ft_model = load_models(ft_model_path)
+    generator = Generator(tokenizer, ft_model)
+    # prompt_template = 'Profil:\nage{age} ; sexe : {sexe} ; lexique: {indication}\nCas clinique:\n'
+    # prompt_template = "<|startoftext|> lexique: {', '.join(antecedents)},"
+    prompt_template = "<|startoftext|> lexique: {antecedents},"
+
+    # Measure chances that our fine-tuned model generates consequents when prompting antecedents
+    df_measures = measure_chances_generating_target(
+        generator=generator,
+        df=df_rules,
+        target_column='consequents',
+        prompt_template=prompt_template,
+        nb_tries=nb_tries,
+        max_new_tokens=max_new_tokens,
+        step=step,
+        block_size=block_size
+        )
+    
+    # Plot the results
+    violin_plot_figure = violin_plot(
+        df_measures,
+        'consequents',
+        'Violin Plots of consequents founds for each prompted antecedents\n(over 30 tries)'
+    )
+    
+    # Save the results
+    save_path = os.path.join(OUTPUT_PATH, experiment_name)
+    os.makedirs(save_path, exist_ok=True)
+    df_measures.to_csv(os.path.join(save_path,'measures.csv'))
+    violin_plot_figure.savefig(os.path.join(save_path, 'violin_plot.png'))
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Measure to which extent a model can generate consequents when prompting antecedents')
+    parser.add_argument('-p', '--path_rules', type=str, required=True, help='Path to the association rules csv file to study')
+    parser.add_argument('-m', '--ft_model_path', type=str, default=FT_MODEL_PATH, help='Path to the fine-tuned model')
+    parser.add_argument('-n', '--number_tries', type=int, default=30, help='Number of generations for each studied case.')
+    parser.add_argument('-t', '--max_tokens', type=int, default=300, help='Number of new tokens.')
+    parser.add_argument('-s', '--step', type=int, default=1, help='Step size between blocks.')
+    parser.add_argument('-b', '--block_size', type=int, default=1, help='Block size')
+    parser.add_argument('-e', '--experiment_name', type=str, required=True)
+    args = parser.parse_args()
+
+    association_rules_path, experiment_name, nb_tries, max_new_tokens, step, block_size, ft_model_path, = args.path_rules, args.experiment_name, args.number_tries, args.max_tokens, args.step, args.block_size, args.ft_model_path
+
+    main(association_rules_path, experiment_name, nb_tries, max_new_tokens, step, block_size, ft_model_path)
---
+++ csc_lib/evaluation.py
@@ -0,0 +1,51 @@
+from tqdm import tqdm
+from csc_lib.config import GEN_ARGS
+
+def measure_chances_generating_target(generator, df, target_column, prompt_template, nb_tries, max_new_tokens, step=1, block_size=1):
+    """
+    Iterates through rows of a dataframe and keeps track of how many information from a target column we can obtain when using prompt_template in the limit of nb_tries tries.
+    
+    Example usage :
+    measure_chances_generating_target(
+        generator = generator,
+        df = df,
+        target_column = 'annotations',
+        prompt_template = 'Profil:\nage{age} ; sexe : {sexe} ; lexique: {indication}\nCas clinique:\n',
+        nb_tries = 30,
+        max_new_tokens = 1300)
+    """
+
+    # Check if the target column is in the dataframe
+    if target_column not in df.columns:
+        raise ValueError(f"The column '{target_column}' is not present in the dataframe.")
+
+    df[f'# {target_column} found each try'] = None
+    df[f'# {target_column} found each try'] = df[f'# {target_column} found each try'].astype(object)
+    df[f'Max {target_column} found'] = None
+    df[f'Max {target_column} found'] = df[f'Max {target_column} found'].astype(object)
+
+    for bloc_idx in tqdm(range(0, len(df)-block_size, step), desc='Iterating through blocks...'):
+
+        for i, row in df[bloc_idx:bloc_idx + block_size].iterrows():
+
+            # prompt = prompt_template.format(**row)
+            antecedents = ', '.join(row['antecedents'])
+            prompt = prompt_template.format(antecedents=antecedents)
+            targets = row[target_column]
+            nb_targets_found_over_each_try = []
+            max_targets_found = []
+
+            for _ in range(nb_tries):
+
+                generated = generator.complete_prompt(prompt, GEN_ARGS, max_new_tokens)
+                found_targets = {target for target in targets if generated.find(target)!=-1}
+                nb_targets_found = len(found_targets)
+                nb_targets_found_over_each_try.append(nb_targets_found)
+
+                if nb_targets_found >= len(max_targets_found):
+                    max_targets_found = found_targets
+
+            df.at[i, f'# {target_column} found each try'] = nb_targets_found_over_each_try
+            df.at[i, f'Max {target_column} found'] = max_targets_found
+
+    return df
