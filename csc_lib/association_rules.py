2e7e6019e145caa8c2d7df702422a452343c601a
FoucauldE <foucauld.estignard@edu.devinci.fr>
2024-06-27 16:31:38 +0200

    only keep most general function

--- csc_lib/association_rules.py
+++ csc_lib/association_rules.py
@@ -31,14 +31,3 @@ def get_associations(set_all_anns:set, anns_dict:dict, min_docs:int, min_confide
     rules = association_rules(frequent_itemsets, min_threshold=min_confidence)
 
     return rules
-
-"""
-def old_get_associations(filenames:list, set_all_anns, dico_anns, min_docs, min_confidence):
-
-    encoded_anns = [{ann: (ann in dico_anns[filename]) for ann in set_all_anns} for filename in filenames]
-    one_hot_df = pd.DataFrame(encoded_anns)
-    min_support = min_docs / len(filenames)
-    frequent_itemsets = fpgrowth(one_hot_df, min_support=min_support, use_colnames=True)
-    rules = association_rules(frequent_itemsets, min_threshold=min_confidence)
-    return rules
-"""
