1124f3e2fd225edeeb21180f1b31c423fa14e46d
FoucauldE <foucauld.estignard@edu.devinci.fr>
2024-06-27 16:32:19 +0200

    remove csv loader & add models loaders

--- csc_lib/data_loader.py
+++ csc_lib/data_loader.py
@@ -1,8 +1,22 @@
 import pandas as pd
-from csc_lib.config import TRAIN_PATH, VAL_PATH, GEN_PATH
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftModel
 
-def load_data():
-    df_train = pd.read_json(TRAIN_PATH)
-    df_val = pd.read_json(VAL_PATH)
-    df_gen = pd.read_json(GEN_PATH)
-    return df_train, df_val, df_gen
+
+def load_models(model_path, base_model_name='bigscience/bloom-1b1'):
+
+    # Load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(base_model_name)
+
+    # Load base model
+    base_model = AutoModelForCausalLM.from_pretrained(base_model_name)
+
+    # Load fine-tuned model
+    ft_model = AutoModelForCausalLM.from_pretrained(base_model_name)
+    ft_model = PeftModel.from_pretrained(ft_model, model_path)
+
+    # Move models to CUDA
+    base_model.to('cuda')
+    ft_model.to('cuda')
+
+    return tokenizer, base_model, ft_model
