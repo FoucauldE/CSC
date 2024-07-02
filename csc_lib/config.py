b1d6050e4be904bb8c2c869430130db5ef4c4666
FoucauldE <foucauld.estignard@edu.devinci.fr>
2024-07-02 17:04:58 +0200

    add models and params for MIA

--- csc_lib/config.py
+++ csc_lib/config.py
@@ -1,3 +1,6 @@
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+
 DATA_PATH = "DATASET/corpus_propre"
 OUTPUT_PATH = "Outputs"
 FT_MODEL_PATH = "../../model/bloom-1b1_lora"
@@ -46,4 +49,26 @@ GEN_ARGS = {
     "max_new_tokens": 200,
     "return_dict_in_generate": True,
     "output_scores": True,
+}
+
+
+models_and_params = {
+    
+    'SVC': {
+        'model': SVC(probability=True, random_state=42),
+        'param_grid': {
+            'model__C': [0.1, 1, 10],
+            'model__gamma': [0.001, 0.01, 0.1]
+        }
+    },
+
+    'xgb': {
+        'model': XGBClassifier(eval_metric='logloss', random_state=42),
+        'param_grid': {
+            'model__n_estimators': [100, 200, 300],
+            'model__max_depth': [3, 5, 7],
+            'model__learning_rate': [0.01, 0.1, 0.3],
+            'model__subsample': [0.6, 0.8, 1.0]
+        }
+    }
 }
