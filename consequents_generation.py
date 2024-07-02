59aeaf41c258f04020483d10c60b7a0d59e2ca2c
FoucauldE <foucauld.estignard@edu.devinci.fr>
2024-07-02 17:04:08 +0200

    correct df loading & ft_path argument

--- consequents_generation.py
+++ consequents_generation.py
@@ -14,8 +14,8 @@ def main(association_rules_path, experiment_name, nb_tries, max_new_tokens, step
     if not os.path.isfile(association_rules_path):
         raise FileNotFoundError(f"The specified path for association rules does not exists: {association_rules_path}")
     
-    # Load annotation rules
-    df_rules = pd.read_csv(association_rules_path)
+    # Load association rules
+    df_rules = pd.read_csv(association_rules_path, index_col=0)
     df_rules = correct_literal_eval(df_rules, ['antecedents', 'consequents'])
 
     # Prepare fine-tuned model to complete new prompts
@@ -55,7 +55,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Measure to which extent a model can generate consequents when prompting antecedents')
     parser.add_argument('-p', '--path_rules', type=str, required=True, help='Path to the association rules csv file to study')
-    parser.add_argument('-m', '--ft_model_path', type=str, default=FT_MODEL_PATH, help='Path to the fine-tuned model')
+    parser.add_argument('-ft', '--ft_model_path', type=str, default=FT_MODEL_PATH, help='Path to the fine-tuned model')
     parser.add_argument('-n', '--number_tries', type=int, default=30, help='Number of generations for each studied case.')
     parser.add_argument('-t', '--max_tokens', type=int, default=300, help='Number of new tokens.')
     parser.add_argument('-s', '--step', type=int, default=1, help='Step size between blocks.')
